10b4f23b8589c54a7c0c082ced2887b632518e04
Carlos Alberto Graniel Córdova <caco572@gmail.com>
2017-10-03 16:28:37 -0500

    add RegisterController

---
+++ EmployeesService/EmployeesService/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeesService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [Authorize]
+        public IEnumerable<Employees> Get()
+        {
+            using (EmployeesDBEntities entities = new EmployeesDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
